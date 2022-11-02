348ee4d1b3fa62298b55bc202da472131438c4d2
Wilson <willythewhale8@gmail.com>
2022-11-02 18:39:31 -0400

    change

--- models/studentModel.js
+++ models/studentModel.js
@@ -5,6 +5,33 @@ const studentSchema = new Schema({
     firstName : {
         type : String,
         required : true
+    },
+    lastName : {
+        type : String,
+        required : true
+    },
+    grade : {
+        type : Number,
+        required : true
+    },
+    idNum : {
+        type : Number,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    points : {
+        type : Number,
+        default : 0
+    },
+    sumPoints : {
+        type : Number,
+        default : 0
+    },
+    eventsAttended : {
+        type : String,
     }
 })
 
