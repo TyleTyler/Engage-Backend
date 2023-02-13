c9d6ee6ee99572e783403ae9b4b53f416b177419
Wilson <willythewhale8@gmail.com>
2023-02-13 12:24:44 -0500

    CORS policy Fixed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const dbUri = process.env.DBURI
 const bp = require('body-parser')
 const cors = require('cors')
 app.use(cors({
-    origin:"http://localhost:3000"
+    origin:"http://127.0.0.1:3000"
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
